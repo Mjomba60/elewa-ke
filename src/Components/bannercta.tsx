abdf6746470aad3ab7430e1a98bb1c33ff5885c3
Mjomba60 <geosam2708@gmail.com>
2023-11-29 19:33:34 +0300

    💄 new component calltoaction button

---
+++ src/Components/bannercta.tsx
@@ -0,0 +1,11 @@
+export default function BannerCTA(){
+
+    return (
+        <div id="ctabaner">
+            <section>
+                <span>Become part of our mission </span>
+                <span>and  <a id="banerlink" href="/invest">join elewa.</a></span>
+            </section>
+        </div>
+    )
+}
