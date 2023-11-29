79d69f30f701eb21435444e6f1a898096250684c
Mjomba60 <geosam2708@gmail.com>
2023-11-29 20:09:55 +0300

    💄 updated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Components/layout';
+import About from './pages/about';
+import Invest from './pages/invest';
+import SocialImpact from './pages/socialImpact';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path= "/" element= {<Layout />}>
+              <Route 
+                index element = {<About />}/>
+              <Route 
+                path='/social-impact' element= {<SocialImpact />}/>
+              <Route 
+                path='/invest' element= {<Invest />}/>
+            </Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
