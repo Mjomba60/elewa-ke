fdbbcc70fb257821bbc33629609254b5dd3308c7
Mjomba60 <geosam2708@gmail.com>
2023-12-01 19:05:30 +0300

    ✨ hnadling click on hamburger

--- src/Components/hamburgermenu.tsx
+++ src/Components/hamburgermenu.tsx
@@ -1,24 +1,50 @@
+import React, { useRef } from "react"
 import { Link } from "react-router-dom"
 
 function Hamburger(){
+
+    let menuref = useRef<HTMLElement | null>(null)
+
+    function burgerClick(){
+        menuref.current!!.classList.toggle('show')
+    }
+
     return(
-        <div id="hamburger">
+    <>
+        <div id="hamburger" onClick={burgerClick}>
             <span></span>
             <span></span>
         </div>
+        <Menu ref = {menuref}/>
+    </>
     )
 }
 
-function NavMenu(){
+const Menu =  React.forwardRef<HTMLElement>((props, ref:any) =>{
+
+    const navigation_links : NavLink[] = [
+        {link: "social-impact", text: "Social Impact"},
+        {link: "/", text: "About"},
+        {link: "invest", text: "Invest"}
+    ]
+
+    function nav_click(){
+        ref!!.current.classList.toggle('show')
+    }
+
+
+
     return (
-        <div id="nav-menu">
-            <nav>
-                <Link to= "social-impact">Social Impact</Link>
-                <Link to= "/">About</Link>
-                <Link to= "invest">Invest</Link>
+            <nav className="nav-menu" ref={ref}>
+                {navigation_links.map((el) => <Link key={el.link} to={el.link} onClick={nav_click}>{el.text}</Link>)}
             </nav>
-        </div>
+
     )
+})
+
+interface NavLink{
+    link: string,
+    text: string
 }
 
-export {Hamburger, NavMenu}
+export {Hamburger, Menu}
