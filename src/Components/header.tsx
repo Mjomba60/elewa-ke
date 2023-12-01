15d55f4c369acb5553e3021019ee042c730cb378
Mjomba60 <geosam2708@gmail.com>
2023-12-01 20:47:48 +0300

    💄 responsive header for different screens

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom";
 import logo from "../Assets/Elewa-logo.svg"
 import { Hamburger } from "./hamburgermenu";
+import { useEffect, useState } from "react";
 
 export default function Header(){
 
-    const scrnwidth: number = window.screen.width
-    const nav = scrnwidth < 426 ? <Hamburger /> :  <nav>
+    const [scrWidth, seScrnWidth] = useState(0)
+
+    useEffect(() => {
+        function handleResize(){
+            seScrnWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return ()=> {
+            window.removeEventListener('resize', handleResize)
+        }
+    },[scrWidth])
+    
+
+    const nav = scrWidth >= 500 ? <nav>
     <Link to= "social-impact">Social Impact</Link>
     <Link to= "/">About</Link>
     <Link to= "invest">Invest</Link>
-</nav>
+        </nav> : <Hamburger />
 
     return (
         <div id="elewa-header">
