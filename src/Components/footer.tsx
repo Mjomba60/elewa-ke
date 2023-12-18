dab2ce02784e340aae7315e42b95501a9be8a888
Mjomba60 <geosam2708@gmail.com>
2023-12-18 21:55:24 +0300

    Updated navigation for footer

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -8,23 +8,27 @@ export default function Footer(){
         <div id="elewa-footer">
             <div id="footer-container">
                 <div className="footer-text">
+                    <a href="https://goo.gl/maps/qvfSXLhuTJzxX7j29">
                     <h4>Headquaters</h4>
                     <p>The Promenade, 19 General</p>
                     <p>Mathenge Rd</p>
                     <p>Nairobi Kenya</p>
+                    </a>
                 </div>
 
                 <div className="footer-text">
+                    <a href="tel:+254.78.92.27.755">
                     <h4>Contacts</h4>
                     <p>T +254 78 92 27 755</p>
                     <p>E info Kenya</p>
+                    </a>
                 </div>
 
                 <div className="footer-text">
                     <h4>Navigation</h4>
-                    <p>Social Impact</p>
-                    <p>About Us</p>
-                    <p>Invest</p>
+                    <p><a href="/social-impact">Social Impact</a></p>
+                    <p><a href="/about-us">About Us</a></p>
+                    <p><a href="/invest">Invest</a></p>
                 </div>
 
                 <div className="footer-text">
